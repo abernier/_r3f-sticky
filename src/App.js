990c5b4d28986da7b7783b0240e27befa2225cea
Antoine BERNIER <antoine.bernier@gmail.com>
2022-10-02 10:17:56 +0200

    onFrame

--- src/App.js
+++ src/App.js
@@ -77,16 +77,19 @@ export default function App() {
   const [numbirds, setNumbirds] = useState(1)
   const [camNth, setCamNth] = useState(0)
 
+  const w = 2 / 100
+  const h = 2 / 100
+
   return (
     <group
       onClick={() => {
         console.log('click')
         setNumbirds(numbirds + 1)
-        // setCamNth(camNth + 1)
+        setCamNth(camNth + 1)
       }}>
-      <PerspectiveCamera makeDefault={camNth % 3 === 0} position={[0, 0, 50]} fov={50}></PerspectiveCamera>
-      <PerspectiveCamera makeDefault={camNth % 3 === 1} position={[0, 0, 30]} fov={90}></PerspectiveCamera>
-      <OrthographicCamera makeDefault={camNth % 3 === 2} near={0.00001}></OrthographicCamera>
+      <PerspectiveCamera makeDefault={camNth % 2 === 0} position={[0, 0, 50]} fov={50}></PerspectiveCamera>
+      <PerspectiveCamera makeDefault={camNth % 2 === 1} position={[0, 0, 30]} fov={90}></PerspectiveCamera>
+      {/* <OrthographicCamera makeDefault={camNth % 3 === 2} near={0.00001}></OrthographicCamera> */}
       {/* <OrthographicCamera makeDefault zoom={5}></OrthographicCamera> */}
 
       <Suspense fallback={null}>
@@ -96,15 +99,23 @@ export default function App() {
           <EdgesItem
             key={i}
             // camera={bool ? persRef.current : orthRef.current}
-            render={(W, H) => {
-              // console.log('WH', W, H)
-              return (
-                <mesh onPointerOver={({ object }) => object.scale.set(2, 2, 2)} onPointerOut={({ object }) => object.scale.set(1, 1, 1)}>
-                  <planeGeometry args={[(2 / 100) * W, (2 / 100) * W]} />
-                  <meshBasicMaterial color={'green'} />
-                </mesh>
-              )
-            }}>
+            // render={({ W, H }) => {
+            //   // console.log('WH', W, H)
+            //   return (
+            //     <mesh onPointerOver={({ object }) => object.scale.set(2, 2, 2)} onPointerOut={({ object }) => object.scale.set(1, 1, 1)}>
+            //       <planeGeometry args={[w * W, h * W]} />
+            //       <meshBasicMaterial color={'green'} />
+            //     </mesh>
+            //   )
+            // }}
+            // insideAlso
+            // onFrame={({ x, y, pin, pinIn, W, H }) => {
+            //   // console.log((pos.x * 2) / 100)
+
+            //   pinIn.position.setX((-x * w * W) / 2)
+            //   pinIn.position.setY((-y * h * W) / 2)
+            // }}
+          >
             <RandBird x={32.43157638924359} y={2.1634717810210837} z={4.896611046209522} bird="Stork" speed={5} factor={1.023085260486265} />
           </EdgesItem>
         ))}
--- src/Edges.js
+++ src/Edges.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import React, { createContext, useContext, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { useThree, useFrame } from '@react-three/fiber'
-import { Vector3 } from 'three'
+import { Box3, Sphere } from 'three'
 
 const EdgesContext = createContext()
 
@@ -9,10 +9,32 @@ function clamp(number, min, max) {
   return Math.max(min, Math.min(number, max))
 }
 
-function EdgesItem({ children, render }) {
-  const { camera } = useThree()
+const a = 0.02 // size of the pin box (in %)
 
+EdgesItem.defaultProps = {
+  render: ({ W, H }) => {
+    // console.log('WH', W, H)
+    return (
+      <mesh onPointerOver={({ object }) => object.scale.set(2, 2, 2)} onPointerOut={({ object }) => object.scale.set(1, 1, 1)}>
+        <planeGeometry args={[a * W, a * W]} />
+        <meshBasicMaterial color={'green'} />
+      </mesh>
+    )
+  },
+  onFrame: ({ x, y, pin, pinIn, W, H }) => {
+    pinIn.position.setX((-x * a * W) / 2)
+    pinIn.position.setY((-y * a * W) / 2)
+  },
+  insideAlso: false
+}
+
+function EdgesItem({ children, render, onFrame, insideAlso }) {
+  const [bbox] = useState(new Box3())
+  const [bs] = useState(new Sphere())
+
+  const { camera } = useThree()
   console.log('camera=', camera, camera.near, camera.far, camera.fov)
+
   let H, W
   if (camera.type === 'PerspectiveCamera') {
     H = 2 * camera.near * Math.tan(((camera.fov / 2) * Math.PI) / 180)
@@ -24,12 +46,51 @@ function EdgesItem({ children, render }) {
   } else {
     console.log('not supported camera type')
   }
-  console.log('H/W=', H, W)
+  console.log('W/H=', W, H)
 
   const containerRef = useRef(null)
   const pinRef = useRef(null)
+  const pinInRef = useRef(null)
+
+  useFrame(() => {
+    console.log('useFrame')
+    const { current: target } = containerRef
+    target.updateWorldMatrix(true, true)
+    bbox.setFromObject(target)
+
+    // bbox.getCenter(center)
+    // console.log('center=', center)
+    bbox.getBoundingSphere(bs)
+
+    let projectedCenter = bs.center.project(camera)
+    // console.log('projectedCenter=', projectedCenter)
+
+    let x, y, z
+    x = clamp(projectedCenter.x, -1, 1)
+    y = clamp(projectedCenter.y, -1, 1)
+    z = clamp(projectedCenter.z, -1, 1)
+
+    const { current: pin } = pinRef
+    // console.log('pin=', pin.position)
+    pin.position.setX((x * W) / 2)
+    pin.position.setY((y * H) / 2)
+
+    // Visible only if offscreen (or explicit `insideAlso` option)
+    pin.visible = insideAlso || Math.abs(projectedCenter.x) > 1 || Math.abs(projectedCenter.y) > 1
+
+    const { current: pinIn } = pinInRef
+    onFrame({
+      pin,
+      pinIn,
+      W,
+      H,
+      x,
+      y
+    })
+  })
 
   useEffect(() => {
+    console.log('useEffect')
     // console.log('camera changed', camera.position.z)
 
     const { current: pin } = pinRef
@@ -39,41 +100,14 @@ function EdgesItem({ children, render }) {
     return () => camera.remove(pin)
   }, [camera])
 
-  useFrame(
-    (function () {
-      const bbox = new THREE.Box3()
-      const bs = new THREE.Sphere()
-
-      return function () {
-        const { current: target } = containerRef
-        target.updateWorldMatrix(true, true)
-        bbox.setFromObject(target)
-
-        // bbox.getCenter(center)
-        // console.log('center=', center)
-        bbox.getBoundingSphere(bs)
-
-        let projectedCenter = bs.center.project(camera)
-        // console.log('projectedCenter=', projectedCenter)
-
-        let x, y, z
-        x = clamp(projectedCenter.x, -1, 1)
-        y = clamp(projectedCenter.y, -1, 1)
-        z = clamp(projectedCenter.z, -1, 1)
-
-        const { current: pin } = pinRef
-        // console.log('pin=', pin.position)
-        pin.position.setX((x * W) / 2)
-        pin.position.setY((y * H) / 2)
-      }
-    })()
-  )
-
   return (
     <>
       <group ref={containerRef}>{children}</group>
-      <group ref={pinRef}>{render(W, H)}</group>
+      <group ref={pinRef}>
+        <group ref={pinInRef}>{render({ W, H })}</group>
+      </group>
     </>
   )
 }
+
 export { EdgesItem }
